b43e23768076ec6ba58bceff47d262dc9e5d7c25
moka <mokaaustine23@gmail.com>
2024-10-07 17:41:03 +0300

    last added functionalities to see if first guess ans is less than 11 and greater than 0

--- game.java
+++ game.java
@@ -8,105 +8,194 @@ class game {
         Scanner scanner = new Scanner(System.in);
 
         Random random = new Random();
-        int random_num = 1+random.nextInt(11);
+        int random_num = 1+random.nextInt(10);
+        System.out.println(random_num);
 
-        System.out.print("Guess a number from 1 to 10: ");
-        int guess = scanner.nextInt();
-        int num_guess = 0;
+        firstGuess: while(true) {
+            System.out.print("Guess a number from 1 to 10: ");
+            int guess = scanner.nextInt();
 
-        while(num_guess < 3 ) {
+            int num_guess = 0;
 
-            boolean lessThan = guess < random_num;
-            boolean greaterThan = guess > random_num;
+            if(guess < 11 && guess > 0) {
+                while(num_guess < 3 ) {
 
-            // user guess one is less than answer
-            if(lessThan) {
-                num_guess++;
-                System.out.print("Secret number is greater than " + guess + ". Please enter a higher number to guess correctly: ");
-                int userGuessTwo = scanner.nextInt();
+                    boolean lessThan = guess < random_num;
+                    boolean greaterThan = guess > random_num;
 
-                // user guess two is greater than answer
-                if(userGuessTwo < random_num) {
-                    num_guess++;
-                    System.out.print("Secret number is still less than " + guess + ". Please enter a lower number to guess correctly: ");
-                    int userGuessThree = scanner.nextInt();
+                    // user guess one is less than answer
+                    if(lessThan) {
+                        num_guess++;
+                        System.out.print("Secret number is greater than " + guess + ". Please enter a higher number to guess correctly: ");
+                        int userGuessTwo = scanner.nextInt();
 
-                    // user guess three is greater than answer
-                    if(userGuessThree > random_num) {
-                        num_guess = 0;
-                        System.out.print("Sorry you have 3 wrong guesses.");
-                        break;
+                        // user guess two is less than answer
+                        if(userGuessTwo < random_num) {
+                            num_guess++;
+                            System.out.print("Secret number is greater than " + userGuessTwo + ". Please enter a higher number to guess correctly: ");
+                            int userGuessThree = scanner.nextInt();
+
+                            // user guess three is greater than answer
+                            if(userGuessThree > random_num) {
+                                num_guess = num_guess + 2;
+                                System.out.print("Sorry you have 3 wrong guesses.");
+                                break;
+                            }
+
+                            // user guess three is less than answer
+                            else if (userGuessThree < random_num) {
+                                num_guess = num_guess + 2;
+                                System.out.print("Sorry you have 3 wrong guesses.");
+                                break firstGuess;
+                            }
+
+                            //  user guess three is the correct answer
+                            else {
+                                System.out.print("Congratulations, your guess number " + random_num + " is correct.");
+                                num_guess = 0;
+                                break  ;
+                            }
+                        }
+
+                        // user guess two is greater than answer============================================================
+                        else if (userGuessTwo > random_num) {
+                            num_guess++;
+                            System.out.print("Secret number is less than " + userGuessTwo + ". Please enter a lower number to guess correctly: ");
+                            int userGuessThree = scanner.nextInt();
+
+                            // user guess three is greater than answer
+                            if(userGuessThree > random_num) {
+                                num_guess = num_guess + 2;
+                                System.out.print("Sorry you have 3 wrong guesses.");
+                                break;
+                            }
+
+                            // user guess three is less than answer
+                            else if (userGuessThree < random_num) {
+                                num_guess = num_guess + 2;
+                                System.out.print("Sorry you have 3 wrong guesses.");
+                                break;
+                            }
+
+                            //  user guess three is the correct answer
+                            else {
+                                System.out.print("Congratulations, your guess number " + userGuessThree + " is correct.");
+                                num_guess = 0;
+                                break  ;
+                            }
+                        }
+
+                        //// user guess two is the correct  answer=============================================================
+                        else {
+                            System.out.print("Congratulations, your guess number " + userGuessTwo + " is correct.");
+                            num_guess = 0;
+                            break  ;
+                        }
+
                     }
 
-                    // user guess three is less than answer
-                    else if (userGuessThree < random_num) {
-                        num_guess = 0;
-                        System.out.print("Sorry you have 3 wrong guesses.");
-                        break;
-                    }
 
-                    //  user guess three is the correct answer
+                    // user guess one is greater than answer
+                    else if(greaterThan) {
+                        num_guess++;
+                        System.out.print("Secret number is less than " + guess + ". Please enter a lower number to guess correctly: ");
+                        int userGuessTwo = scanner.nextInt();
+
+                        // user guess two is less than answer
+                        if(userGuessTwo < random_num) {
+                            num_guess++;
+                            System.out.print("Secret number is greater than " + userGuessTwo + ". Please enter a higher number to guess correctly: ");
+                            int userGuessThree = scanner.nextInt();
+
+                            // user guess three is greater than answer
+                            if(userGuessThree > random_num) {
+                                num_guess = num_guess + 2;
+                                System.out.print("Sorry you have 3 wrong guesses.");
+                                break;
+                            }
+
+                            // user guess three is less than answer
+                            else if (userGuessThree < random_num) {
+                                num_guess = num_guess + 2;
+                                System.out.print("Sorry you have 3 wrong guesses.");
+                                break;
+                            }
+
+                            //  user guess three is the correct answer
+                            else {
+                                System.out.print("Congratulations, your guess number " + random_num + " is correct.");
+                                num_guess = 0;
+                                break  ;
+                            }
+                        }
+
+                        // user guess two is greater than answer============================================================
+                        else if (userGuessTwo > random_num) {
+                            num_guess++;
+                            System.out.print("Secret number is less than " + userGuessTwo + ". Please enter a lower number to guess correctly: ");
+                            int userGuessThree = scanner.nextInt();
+
+                            // user guess three is greater than answer
+                            if(userGuessThree > random_num) {
+                                num_guess = num_guess + 2;
+                                System.out.print("Sorry you have 3 wrong guesses.");
+                                break;
+                            }
+
+                            // user guess three is less than answer
+                            else if (userGuessThree < random_num) {
+                                num_guess = num_guess + 2;
+                                System.out.print("Sorry you have 3 wrong guesses.");
+                                break;
+                            }
+
+                            //  user guess three is the correct answer
+                            else {
+                                System.out.print("Congratulations, your guess number " + userGuessThree + " is correct.");
+                                num_guess = 0;
+                                break  ;
+                            }
+                        }
+
+                        //// user guess two is the correct  answer=============================================================
+                        else {
+                            System.out.print("Congratulations, your guess number " + userGuessTwo + " is correct.");
+                            num_guess = 0;
+                            break  ;
+                        }
+
+
+                    }
+                    // user guess one is correct answer
                     else {
-                        System.out.print("Congratulations, your guess number " + random_num + " is correct.");
-                        num_guess = 0;
-                        break;
+                        System.out.print("Congratulations, for guessing the number " + guess + " on your first trial.");
+                        //num_guess = 0;
+                        break firstGuess;
                     }
+
+
+
                 }
 
-            }
-
-
-            // user guess one is greater than answer
-            else if(greaterThan) {
-                num_guess++;
-                System.out.print("Secret number is less than " + guess + ". Please enter a lower number to guess correctly: ");
-                int greaterThanAnsTwo = scanner.nextInt();
-
-                // user guess two is greater than answer
-                if(greaterThanAnsTwo > random_num) {
-                    num_guess++;
-                    System.out.print("Secret number is still less than " + guess + ". Please enter a lower number to guess correctly: ");
-                    int greaterThanAnsThree = scanner.nextInt();
-
-                    // user guess three is greater than answer
-                    if(greaterThanAnsThree > random_num) {
-                        num_guess = 0;
-                        System.out.print("Sorry you have 3 wrong guesses.");
-                        break;
-                    }
-
-                    // user guess three is less than answer
-                    else if (greaterThanAnsThree < random_num) {
-                        num_guess = 0;
-                        System.out.print("Sorry you have 3 wrong guesses.");
-                        break;
-                    }
-
-                    //  user guess three is the correct answer
-                    else {
-                        System.out.print("Congratulations, your guess number " + random_num + " is correct.");
-                        num_guess = 0;
-                        break;
-                    }
+                if(num_guess > 2){
+                    System.out.println(" The secret number is " + random_num + ".\nWe wish you luck next time.");
+                    random_num = 1+random.nextInt(10);
                 }
-
             }
-            // user guess one is correct answer
+            /// ===================================begin from here======================================================================================
             else {
-                System.out.print("Congratulations, your guess number " + random_num + " is correct.");
-                num_guess = 0;
-                break;
+                System.out.print("Please enter a number between 1 to 10: ");
+                int notValidInput = scanner.nextInt();
+
+                int returning_notValidInput (){
+                    int ans = notValidInput;
+                    return ans;
+                }
             }
 
 
-
         }
 
-        if(num_guess > 2){
-            System.out.print("Sorry you are unable to correctly guess the number which is " + random_num + ".");
-            System.out.print("We wish you luck next time.");
-            num_guess = 0;
-        }
         
     }
 }
